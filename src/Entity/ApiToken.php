d025cc415e1a44a3545aeba3d79767666310d0b3
carlosvieira student <carlosvieira278@gmail.com>
2023-06-07 15:37:32 +0200

    docker

---
+++ src/Entity/ApiToken.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ApiTokenRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ApiTokenRepository::class)]
+class ApiToken
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
